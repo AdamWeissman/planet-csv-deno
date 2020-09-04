a3cdba516f8627436d6d799db29979b78a4a8a17
AdamWeissman <adam@comedytragedyepic.com>
2020-09-03 22:26:20 -0400

    walkthrough covering bufreder and parse

--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 
 1. Ensure you have Deno installed: https://deno.land/
 2. In the terminal, run: `deno run --allow-read mod.ts`
-
-testing.
--- testing.ts
+++ testing.ts
@@ -1,11 +1,17 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts"
 
+async function loadPlanetsData() {
+  const path = join(".", "kepler_exoplanets_nasa.csv");
+  const file = await Deno.open(path)
+  const bufReader = new BufReader(file)
+  const result = await parse(bufReader, {
+    header: true,
+    comment: "#",
+  });
 
-async function readFile() {
-  const path = join("text_files", "hello.txt");
-  const data = await Deno.readTextFile(path)
-
-  console.log(data);
+  console.log(result)
 }
 
-await readFile();
+await loadPlanetsData();
